3557df40ae15ce24733fead0a4944fcbe9f529cb
= <=>
2012-09-25 19:19:50 +0200

    Added tests for the results_formatter. ResultsFormatter module needs to be instantiated before use

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var $ = require('jquery');
 
 var switchboard = require('./index');
 
-var localRecipe = require('./example_routines/actor_movies_books.json');
+var localRecipe = require('./example_routines/starwars_artists.json');
 var remoteRecipe = null;
 
 /* loads a JSON-routine from chef */
