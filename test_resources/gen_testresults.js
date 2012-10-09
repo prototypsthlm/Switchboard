789f76f58674b609c55ae3ab1d73c270beb0b780
Niklas Ek <ek.niklas@gmail.com>
2012-10-09 17:10:34 +0200

    genTestResults now writes to file, if passing the crazy jungle of asyncness

--- test_resources/gen_testresults.js
+++ test_resources/gen_testresults.js
@@ -1,46 +1,75 @@
 /*
- * Generate resultfiles for the example routines.
- * 
-*/
+ * Generates resultfiles using the example routines specified below.
+ * Testresults are used for testing the response from apis during development.
+ */
 
-var Switchboard = require('../index');
 
- // Test data
-var testData = {
-	request: {query: {q: "Back to the future"}},
-	queries: {		
-		headliner_biographies: 'Skrillex',
-		starwars_artists: "Titanic",
+/* Test data to be written. Specify each example_routine here. */
+var config = {
+	
+	folderRoutines: '../example_routines/',
+	folderTestResults: './test_resources/',
+
+	routineTestData: {
+		artist_urls: 'xx',
+		actor_movies_books: 'Will Smith',
+		starwars_artists: 'Titanic',
+		headliner_biographies: 'Coldplay'
 	}
 };
 
 
-function routine(routineName) {
-	return require('../example_routines/'+ routineName +'.json');
+
+// REQUIRE
+var fs = require('fs'),
+	Switchboard = require('../index');
+
+
+
+// NAMESPACE
+var GenTestResults = {
+
+	// Initialize the test data generation
+	init: function(config){
+
+		// Loop through routineTestData
+		// Use key as routine
+		// Use value as input data
+		for( var routineName in config.routineTestData ){
+			var query = config.routineTestData[routineName];
+			console.log(query);
+			this.handleOne(routineName, query, config);
+		}
+	},
+
+	// Fetches data for one routine and runs the callback.
+	handleOne: function(routineName, query, config) {
+
+		var routine = require( config.folderRoutines + routineName +'.json' ),
+			jobId = Switchboard.addJob(routine, [query]),
+			self = this;
+
+
+		Switchboard.runJob(jobId, function(usedRoutine, clean, raw){				
+			self.writeResults(usedRoutine, clean, raw, routineName, config.folderTestResults, query);
+		});
+
+	},
+
+	// Writes the data to file, used in handleOne 
+	writeResults: function(usedRoutine, clean, raw, routineName, folderTestResults, query){
+		console.log(routineName);
+		console.log(query);
+		var path = folderTestResults+"results.gen."+routineName+".json";
+		fs.writeFile(path, JSON.stringify(clean, null, 4), function(err) {
+	        if(err) {
+	            console.log(err);
+	        } else {
+	            console.log("Testresults written to " + path);
+	        }
+    	});
+	}
 };
 
-function request(routineName) {
-	return { query: { q: testData.queries[routineName] } };
-};
+GenTestResults.init(config);
 
-function go(routineName) {
-
-	var routine = require('../example_routines/'+ routineName +'.json');
-	var request = "Titanic";
-
-	console.log(routine);
-	console.log(request);
-
-	var jobId = Switchboard.addJob(routine, [request]);
-    // Switchboard.runJob(jobId, function(usedRoutine, clean, raw) { handleResponse(usedRoutine, clean, raw, req, res, allowJSONP); });
-   
-
-	Switchboard.runJob(jobId, function (usedRoutine, clean, raw) {
-		console.log(usedRoutine);
-		console.log(clean);
-		console.log(raw);
-	});
-};
-
-
-go("starwars_artists");
